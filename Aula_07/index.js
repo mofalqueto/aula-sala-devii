0b86f8405f1cb5f848143d261892a167fabb28ef
MONICA AIMOLA FALQUETO <1091392223023@fatec-srv.local>
2023-04-20 20:56:40 -0300

    aula 17/04

--- Aula_07/index.js
+++ Aula_07/index.js
@@ -9,6 +9,7 @@ app.listen(8080, () => {
 });
 //comandar npm start para testar o servidor
 // ctrl + C para parar o servidor no terminal 
+let Alunos = ['Maria', 'Joao', 'Jose'];
 
 //para criar uma rota de post
 
@@ -16,6 +17,40 @@ app.post('/includeAluno', (req, res) => {
     const { nome } = req.body; 
 
     res.send(`<h1> Olá ${nome}, Bem vindo!`);
-
 });
 
+//READ
+
+app.get('/getAlunos', (req, res) => {
+    const { index } = req.body;
+    //connect SQL - 
+    //select * from Alunos Where id = index
+    res.send(`<h1> O aluno ${Alunos[index]} foi encontrado</h1>`);
+});
+
+app.get('/getAlunos', (req, res) => {
+    // select  * from Alunos
+    console.log(Alunos);
+    res.send('Todos os alunos cadastrados são: ${Alunos}')
+});
+
+
+//update
+
+app.put('/updateAluno', (req, res) => {
+    // update  nome from Alunos where id = index;
+    const {index, nome} = req.body;
+    Alunos[index] = nome;
+    res.send("<h1> O nome foi atualizado com sucessp!!!</h1>");
+    console.log(Alunos);
+});
+
+
+//delete
+
+app.delete('/deleteAluno', (req, res) => {
+    // DELETE FROM Alunos WHERE id = index;
+    const { index } = req.body;
+    Alunos.splice(index, 1);
+    res.send(`Alunos apos o DELETE: ${Alunos}`);
+});
